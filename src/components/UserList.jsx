c32792584a429cccd69ac3c7ab3b00b3faaa7426
Kby16 <kbykrishnabalaji@gmail.com>
2025-01-27 17:14:59 +0530

    feat: add new functionality to UserList component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 
 const UserList = ({ users, onEdit, onDelete }) => {
+  
+  
   return (
+    <>
+    {users.length > 0 ? 
     <table border="1" style={{ width: "100%", textAlign: "left" }}>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th> Name</th>
-          <th> Userame</th>
-          <th>Email</th>
-          <th>Website</th>
-          <th>Actions</th>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th> Name</th>
+        <th> Userame</th>
+        <th>Email</th>
+        <th>Website</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {users.map((user) => (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name || "N/A"}</td>
+          <td>{user.username || "N/A"}</td>
+          <td>{user.email || "N/A"}</td>
+          <td>{user.website || "N/A"}</td>
+          <td>
+            <button onClick={() => onEdit(user)}>Edit</button>
+            <button onClick={() => onDelete(user.id)}>Delete</button>
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <tr key={user.id}>
-            <td>{user.id}</td>
-            <td>{user.name || "N/A"}</td>
-            <td>{user.username || "N/A"}</td>
-            <td>{user.email || "N/A"}</td>
-            <td>{user.website || "N/A"}</td>
-            <td>
-              <button onClick={() => onEdit(user)}>Edit</button>
-              <button onClick={() => onDelete(user.id)}>Delete</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      ))}
+    </tbody>
+  </table> : 
+    <h2>There is no list to show. Add to view, use Add User Button</h2>
+    }
+    
+    </>
   );
 };
 
